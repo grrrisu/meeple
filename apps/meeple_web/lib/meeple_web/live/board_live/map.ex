1f05435247837ebac9d902a7b5796e1e3a9fc079
Alessandro Di Maria <adm@m42.ch>
2021-12-13 23:00:42 +0100

    map scroll arrows

--- apps/meeple_web/lib/meeple_web/live/board_live/map.ex
+++ apps/meeple_web/lib/meeple_web/live/board_live/map.ex
@@ -5,15 +5,21 @@ defmodule MeepleWeb.BoardLive.Map do
     # for now we a use the container width lx of 1280px
     # https://tailwindcss.com/docs/container
     ~H"""
-    <div
-      id="map"
-      class="board-map grid place-content-center"
-      style="grid-template-columns: repeat(15, 75px); grid-template-rows: repeat(7, 75px)">
-      <%= for _row <- 0..6 do %>
-        <%= for _column <- 0..14 do %>
-          <div class="field"></div>
+    <div id="map" class="board-map grid place-items-center text-gray-500"
+      style="grid-template-columns: 70px auto 70px; grid-template-rows: 40px auto 40px;">
+      <div style="grid-column: 1 / span 3"><i class="las la-caret-up la-3x"></i></div>
+      <div class="justify-self-end"><i class="las la-caret-left la-3x "></i></div>
+      <div
+        class="grid place-content-center"
+        style="grid-template-columns: repeat(15, 75px); grid-template-rows: repeat(7, 75px)">
+        <%= for _row <- 0..6 do %>
+          <%= for _column <- 0..14 do %>
+            <div class="field"></div>
+          <% end %>
         <% end %>
-      <% end %>
+      </div>
+      <div class="justify-self-start"><i class="las la-caret-right la-3x"></i></div>
+      <div style="grid-column: 1 / span 3"><i class="las la-caret-down la-3x"></i></div>
     </div>
     """
   end
