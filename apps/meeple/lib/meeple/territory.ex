b5ae243d14bd5b0daa952c6aced8d7585322cbda
Alessandro Di Maria <adm@m42.ch>
2021-12-25 00:17:56 +0100

    Territory One

--- apps/meeple/lib/meeple/territory.ex
+++ apps/meeple/lib/meeple/territory.ex
@@ -1,19 +1,13 @@
 defmodule Meeple.Territory do
   use Agent
 
-  alias Sim.Grid
+  alias Meeple.Territory.One
 
   def start_link(_args) do
-    Agent.start_link(&create/0, name: __MODULE__)
+    Agent.start_link(&One.create/0, name: __MODULE__)
   end
 
   def get() do
     Agent.get(__MODULE__, & &1)
   end
-
-  def create(), do: Grid.create(15, 7, &field/2)
-
-  defp field(_x, 0), do: :high_mountain
-  defp field(7, 1), do: :headquarter
-  defp field(_x, _y), do: nil
 end
