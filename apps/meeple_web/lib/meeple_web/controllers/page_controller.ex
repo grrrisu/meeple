40b223c270607a4c60a0933881956db6c920577a
Alessandro Di Maria <adm@m42.ch>
2022-01-02 16:24:22 +0100

    show link to current board only if one exists

--- apps/meeple_web/lib/meeple_web/controllers/page_controller.ex
+++ apps/meeple_web/lib/meeple_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule MeepleWeb.PageController do
   use MeepleWeb, :controller
 
+  alias Meeple.Territory
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    territory_exists = Territory.exists?()
+    render(conn, "index.html", territory_exists: territory_exists)
   end
 end
