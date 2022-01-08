72ae01f7dc860117c23bbe0a2f426fe408126f17
Alessandro Di Maria <adm@m42.ch>
2022-01-08 10:57:05 +0100

    start with an empty field and pass it on click

--- apps/meeple_web/lib/meeple_web/live/board_live/field_card.ex
+++ apps/meeple_web/lib/meeple_web/live/board_live/field_card.ex
@@ -2,28 +2,67 @@ defmodule MeepleWeb.BoardLive.FieldCard do
   use MeepleWeb, :live_component
   require Logger
 
-  alias Meeple.Territory
+  alias MeepleWeb.BoardLive.Map
 
-  def render(assigns) do
+  def render(%{field: nil} = assigns) do
+    ~H"""
+    <div>
+      <.card_border let={nil} innerfield={nil}>
+        <div class="border border-gray-800" style="height: 450px">
+          loading ...
+        </div>
+      </.card_border>
+    </div>
+    """
+  end
+
+  def render(%{field: f} = assigns) do
+    Logger.debug("render field #{inspect(f)}")
+
+    ~H"""
+    <div>
+      <.card_border let={field} innerfield={f}>
+        <div class="border border-gray-800">
+          <h2><%= field[:vegetation] |> Atom.to_string() |> String.capitalize() %></h2>
+          <div class="" style="height: 392px">
+            <div class="my-2 mx-auto">
+              <image src={"/images/fields/#{Map.vegetation_image(field[:vegetation])}"} height="50"/>
+            </div>
+            <p>
+              Description: <%= field[:vegetation] %>
+            </p>
+            <p>
+              <%= if field[:flora] do %>
+                Flora: <%= field[:flora] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:herbivore] do %>
+                Herbivores: <%= field[:herbivore] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:predator] do %>
+                Predators: <%= field[:predator] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:danger] do %>
+                Danger: <%= field[:danger] |> inspect() %><br/>
+              <% end %>
+            </p>
+            <div>
+              Actions available ....
+            </div>
+          </div>
+        </div>
+      </.card_border>
+    </div>
+    """
+  end
+
+  def card_border(assigns) do
     ~H"""
     <div
       class="bg-gray-50 rounded-lg drop-shadow-lg border border-gray-800 p-4 absolute"
       :class="showFieldCard || 'hidden'"
+      x-cloak
       style="width: 300px; top: 30px; right: 210px">
-      <div class="border border-gray-800">
-        <h2>Homebase</h2>
-        <div class="" style="height: 392px">
-          <div class="my-2 mx-auto">
-            <image src="/images/fields/homebase.svg" height="50"/>
-          </div>
-          <p>
-            Description: This is your homebase.
-          </p>
-          <div>
-            Actions available ....
-          </div>
-        </div>
-      </div>
+      <%= render_slot(@inner_block, @innerfield) %>
     </div>
     """
   end
