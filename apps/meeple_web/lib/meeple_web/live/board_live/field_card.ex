b7e656aa8813a79f9ac87ce33563bed6282eb0fa
Alessandro Di Maria <adm@m42.ch>
2022-01-09 22:39:26 +0100

    card title and close button

--- apps/meeple_web/lib/meeple_web/live/board_live/field_card.ex
+++ apps/meeple_web/lib/meeple_web/live/board_live/field_card.ex
@@ -18,30 +18,38 @@ defmodule MeepleWeb.BoardLive.FieldCard do
     ~H"""
     <div>
       <.card_border let={field} innerfield={f}>
-        <h2><%= (field[:building] || field[:vegetation]) |> Atom.to_string() |> String.capitalize() %></h2>
-        <div class="" style="height: 392px">
-          <div class="my-2 mx-auto">
-            <image src={"/images/fields/#{(field[:building] && "homebase.svg") || Map.vegetation_image(field[:vegetation])}"} height="50"/>
+        <div class="mt-5" style="height: 392px">
+          <.card_title title={@field[:building] || @field[:vegetation]} />
+          <div class="my-2 mx-auto grid justify-center bg-steelbluex-100">
+            <image
+              src={"/images/fields/#{(field[:building] && "homebase.svg") || Map.vegetation_image(field[:vegetation])}"}
+              class="border border-steelblue-800"
+              style="height: 240px"/>
           </div>
-          <p>
-            Description: <%= field[:vegetation] %>
-          </p>
-          <p>
-            <%= if field[:flora] do %>
-              Flora: <%= field[:flora] |> inspect() %><br/>
-            <% end %>
-            <%= if field[:herbivore] do %>
-              Herbivores: <%= field[:herbivore] |> inspect() %><br/>
-            <% end %>
-            <%= if field[:predator] do %>
-              Predators: <%= field[:predator] |> inspect() %><br/>
-            <% end %>
-            <%= if field[:danger] do %>
-              Danger: <%= field[:danger] |> inspect() %><br/>
-            <% end %>
-          </p>
-          <div>
-            Actions available ....
+          <div class="px-3 text-sm">
+            <p class="italic">
+              Description: <%= field[:vegetation] %>
+            </p>
+            <p>
+              <%= if field[:flora] do %>
+                Flora: <%= field[:flora] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:herbivore] do %>
+                Herbivores: <%= field[:herbivore] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:predator] do %>
+                Predators: <%= field[:predator] |> inspect() %><br/>
+              <% end %>
+              <%= if field[:danger] do %>
+                Danger: <%= field[:danger] |> inspect() %><br/>
+              <% end %>
+            </p>
+            <div>
+              Actions available ....<br/>
+              Option 1 <br/>
+              Option 2 <br/>
+              Option 3 <br/>
+            </div>
           </div>
         </div>
       </.card_border>
@@ -61,7 +69,33 @@ defmodule MeepleWeb.BoardLive.FieldCard do
       x-transition:leave-end="opacity-0 scale-75">
       <div class="border border-gray-800" style="height: 448px">
         <%= render_slot(@inner_block, @innerfield) %>
-        <div @click="showFieldCard = false" class="text-center">close</div>
+        <.card_close_button text="close" />
+      </div>
+    </div>
+    """
+  end
+
+  def card_title(assigns) do
+    title =
+      assigns.title
+      |> Atom.to_string()
+      |> String.capitalize()
+
+    ~H"""
+    <div class="text-center font-bold absolute text-steelblue-800"
+      style="width: 280px; height: 30px; top: 13px; right: 10px; background-image: url('/images/ui/card_title.svg'); background-size: 280px 30px">
+      <h2 class="pt-1"><%= title %></h2>
+    </div>
+    """
+  end
+
+  def card_close_button(assigns) do
+    ~H"""
+    <div class="grid justify-center">
+      <div class="text-center text-steelblue-800 pt-0.5"
+        style="width: 159px; height: 30px; top: 13px; right: 10px; background-image: url('/images/ui/button.svg'); background-size: 159px 30px"
+        @click="showFieldCard = false">
+        <%= @text %>
       </div>
     </div>
     """
