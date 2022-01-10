f543fa23e3fbc731f8934cf4c93398a9d54bbe8a
Alessandro Di Maria <adm@m42.ch>
2022-01-10 22:28:13 +0100

    use lastest master of phoenix_live_view, disable live_dashbaord temporarely

--- apps/meeple_web/test/meeple_web/live/board_live/map_test.exs
+++ apps/meeple_web/test/meeple_web/live/board_live/map_test.exs
@@ -19,8 +19,6 @@ defmodule MeepleWeb.BoardLive.MapTest do
     assert view |> element("#field-1-1") |> render() =~ "homebase.svg"
   end
 
-  # skip until https://github.com/phoenixframework/phoenix_live_view/issues/1824 is relsolved
-  @tag :skip
   test "discover", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/board")
 
@@ -32,8 +30,6 @@ defmodule MeepleWeb.BoardLive.MapTest do
     assert view |> element("#field-1-2") |> render() =~ "planes.svg"
   end
 
-  # skip until https://github.com/phoenixframework/phoenix_live_view/issues/1824 is relsolved
-  @tag :skip
   test "show clicked field details", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/board")
 
@@ -41,6 +37,6 @@ defmodule MeepleWeb.BoardLive.MapTest do
     |> element("#field-1-2")
     |> render_click()
 
-    assert view |> element("#field-card") |> render() =~ "<h2>Planes</h2>"
+    assert view |> element("#field-card") |> render() =~ "Planes</h2>"
   end
 end
