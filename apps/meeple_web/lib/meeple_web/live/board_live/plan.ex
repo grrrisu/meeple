182374bed82279542f94c737ec145a90c8521bee
Alessandro Di Maria <adm@m42.ch>
2022-01-29 22:17:07 +0100

    show action points

--- apps/meeple_web/lib/meeple_web/live/board_live/plan.ex
+++ apps/meeple_web/lib/meeple_web/live/board_live/plan.ex
@@ -13,24 +13,22 @@ defmodule MeepleWeb.BoardLive.Plan do
   def render(assigns) do
     ~H"""
     <div id="plan" class="plan-wall mt-8" style="margin-left: 80px; margin-right: 80px">
-      <div class="grid grid-cols-3" style="width: 494px; grid-template-columns: 112px repeat(3, 90px) 112px">
+      <div class="grid grid-cols-3 relative" style="width: 494px; grid-template-columns: 112px repeat(10, 90px) 112px">
         <div style="width: 112px; height: 150px;">
           <img class="w-full" src="/images/ui/wall_left.svg" />
         </div>
-        <div class="relative" style="width: 90px; height: 150px;">
-          <img class="w-full absolute" src="/images/ui/action_move.svg" />
+        <%= for _i <- 1..10 do %>
+          <div style="width: 90px; height: 150px;">
           <img class="w-full" src="/images/ui/wall_middle.svg" />
-        </div>
-        <div class="relative" style="width: 90px; height: 150px;">
-          <img class="w-full absolute" src="/images/ui/action_discover.svg" />
-          <img class="w-full" src="/images/ui/wall_middle.svg" />
-        </div>
-        <div style="width: 90px; height: 150px;">
-          <img class="w-full" src="/images/ui/wall_middle.svg" />
-        </div>
+          </div>
+        <% end %>
         <div style="width: 112px; height: 150px;">
           <img class="w-full" src="/images/ui/wall_right.svg" />
         </div>
+
+        <%= for {action, index} <- Enum.with_index(@plan.actions) do %>
+          <.action index={index} action={action} />
+        <% end %>
       </div>
 
       <div>
@@ -40,6 +38,13 @@ defmodule MeepleWeb.BoardLive.Plan do
         <div class="border" phx-click="run" phx-target={@myself}>run</div>
       </div>
     </div>
+    """
+  end
+
+  def action(assigns) do
+    ~H"""
+      <img style={"width: 90px; top: -10px; left: #{@index *90}px"} class="absolute" src={"/images/ui/action_#{@action.name}.svg"} />
+      <img style={"width: 25px; top: 110px; left: #{@index *90 + 30}px"} class="absolute" src={"/images/ui/action_points_#{@action.points - @action.done}.svg"} />
     """
   end
 
