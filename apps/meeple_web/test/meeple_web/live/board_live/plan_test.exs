0ddc79f15e722e302137f6506402b484efca824c
Alessandro Di Maria <adm@m42.ch>
2022-01-30 12:28:20 +0100

    plan view test

--- apps/meeple_web/test/meeple_web/live/board_live/plan_test.exs
+++ apps/meeple_web/test/meeple_web/live/board_live/plan_test.exs
@@ -12,4 +12,25 @@ defmodule MeepleWeb.BoardLive.PlanTest do
     {:ok, view, _html} = live(conn, "/board")
     assert view |> has_element?("#plan")
   end
+
+  test "add action to plan wall", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/board")
+
+    Board.add_action(%{
+      name: :discover,
+      pawn: %{id: 1, x: 1, y: 2},
+      x: 1,
+      y: 2,
+      points: 4,
+      done: 0
+    })
+
+    send(view.pid, {:plan_updated})
+
+    {:ok, view, _html} = live(conn, "/board")
+
+    assert view
+           |> element("#plan")
+           |> render() =~ "action_discover.svg"
+  end
 end
