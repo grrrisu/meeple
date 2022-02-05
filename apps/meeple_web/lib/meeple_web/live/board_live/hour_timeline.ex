51e51e542ac20eb64db9a6c135861937560262f9
Alessandro Di Maria <adm@m42.ch>
2022-02-05 18:20:52 +0100

    no transition when resetting sun

--- apps/meeple_web/lib/meeple_web/live/board_live/hour_timeline.ex
+++ apps/meeple_web/lib/meeple_web/live/board_live/hour_timeline.ex
@@ -17,7 +17,7 @@ defmodule MeepleWeb.BoardLive.HourTimeline do
 
     ~H"""
     <div id="map-hour-timeline" class="w-full h-4/5 overflow-hidden relative grid" style="grid-template-columns: repeat(12, 1fr)">
-      <div class="absolute transition-position duration-[1000ms]" style={"bottom: #{@bottom}%; left: #{@hour/12 * 100}%"}>
+      <div class={"absolute #{sun_transistion(@hour)}"} style={"bottom: #{@bottom}%; left: #{@hour/12 * 100}%"}>
         <img style="margin-left: 8px; width: 30px" src="/images/ui/sun_symbol.svg" />
       </div>
       <%= for i <- 0..11 do %>
@@ -33,14 +33,18 @@ defmodule MeepleWeb.BoardLive.HourTimeline do
     if digit == hour, do: "opacity-0", else: "opacity-100"
   end
 
+  defp sun_transistion(hour) do
+    if hour > 0, do: "transition-position duration-[1000ms]"
+  end
+
   def plan(assigns) do
     top = if assigns.hour == 0 || assigns.hour == 11, do: 12, else: 0
     assigns = assign(assigns, top: top)
 
     ~H"""
     <div id="plan-hour-timeline" class="relative w-full mx-8">
-      <div class="absolute transition-position duration-[1000ms] overflow-hidden" style={"width: 25px; height: 25px; top: 10px; left: #{@hour/12 * 100}%"}>
-        <img class="absolute transition-position  duration-[1000ms]" style={"top: #{@top}px"} src={"/images/ui/sun_symbol.svg"} />
+      <div class={"absolute overflow-hidden #{sun_transistion(@hour)}"} style={"width: 25px; height: 25px; top: 10px; left: #{@hour/12 * 100}%"}>
+        <img class="absolute #{sun_transistion(@hour})}" style={"top: #{@top}px"} src={"/images/ui/sun_symbol.svg"} />
       </div>
     </div>
     """
