bf06504b20b29361478fd26f4fad50578a5fbbd9
Alessandro Di Maria <adm@m42.ch>
2022-03-07 16:53:21 +0100

    load sim modules from thundermoon_umbrella

--- apps/meeple/mix.exs
+++ apps/meeple/mix.exs
@@ -40,7 +40,9 @@ defmodule Meeple.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
-      {:swoosh, "~> 1.3"}
+      {:swoosh, "~> 1.3"},
+      # {:sim, git: "https://github.com/grrrisu/thundermoon-umbrella.git", subdir: "/apps/sim", app: false}
+      {:sim, path: "../../../thundermoon_umbrella/apps/sim", app: false, only: :dev}
     ]
   end
 
