1a2ee21b08e964cebfbaf75a5a360adb3948b539
Alessandro Di Maria <adm@m42.ch>
2022-03-12 10:38:25 +0100

    remove create from board module

--- apps/meeple/lib/meeple/services/user_service.ex
+++ apps/meeple/lib/meeple/services/user_service.ex
@@ -3,20 +3,4 @@ defmodule Meeple.Service.User do
 
   # alias Sim.Realm.SimulationLoop
   alias Meeple.{Tableau, FogOfWar, Pawn, Plan, Territory}
-
-  @impl true
-  def execute(:create, name: name) do
-    :ok = Territory.create(name)
-    :ok = Tableau.create(name)
-    Tableau.pawns() |> Enum.each(&Territory.set_pawn(&1))
-    :ok = FogOfWar.create(name)
-    :ok = Plan.clear()
-    [{:user, :game_created}]
-  end
-
-  def clear_plan() do
-    :ok = Plan.clear()
-    # ???
-    [{:admin, :game_created}]
-  end
 end
